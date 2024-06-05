0b23ecd3918f60b0d24b39c06d526a8933585954
seafoodMujica <shifudo20@gmail.com>
2024-06-05 21:06:52 +0900

    IndexとList画面の作成

---
+++ web-app/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("user/list")
+	public String displayList(Model model) {
+
+		model.addAttribute("id", 100);
+		model.addAttribute("name", "サンプル太郎");
+		model.addAttribute("address", "東京都新宿区1-2-3");
+		model.addAttribute("phone", "090-1234-1234");
+		model.addAttribute("updateDate", LocalDateTime.now());
+
+		return "user/list";
+	}
+}
---
+++ web-app/src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("message", "ようこそ");
+		model.addAttribute("datetime", LocalDateTime.now());
+		return "index";
+	}
+
+}
