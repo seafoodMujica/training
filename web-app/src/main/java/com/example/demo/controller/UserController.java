705b172b151a48b93693d55b14359fcde411b439
seafoodMujica <shifudo20@gmail.com>
2024-06-17 21:08:12 +0900

    詳細画面、一覧画面の改修・調整

--- web-app/src/main/java/com/example/demo/controller/UserController.java
+++ web-app/src/main/java/com/example/demo/controller/UserController.java
@@ -1,38 +1,38 @@
 package com.example.demo.controller;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
 @Controller
 public class UserController {
 
-	@GetMapping("/user/list")
+	@Autowired
+	UserService userService;
+
+	@GetMapping(value = "/user/list")
 	public String displayList(Model model) {
-
-		model.addAttribute("id", 100);
-		model.addAttribute("name", "サンプル太郎");
-		model.addAttribute("address", "東京都新宿区1-2-3");
-		model.addAttribute("phone", "090-1234-1234");
-		model.addAttribute("updateDate", LocalDateTime.now());
-		model.addAttribute("createDate", "2024/05/15 12:00:00");
-
+		List<User> userlist = userService.searchAll();
+		model.addAttribute("userlist", userlist);
 		return "user/list";
 	}
 
-	@GetMapping("/user/view/{id}")
-	public String displayView(@PathVariable Long id, Model model) {
+	@GetMapping(value = "/user/add")
+	public String displayAdd(Model model) {
+		return "user/add";
+	}
 
-		model.addAttribute("id", id);
-		model.addAttribute("name", "サンプル太郎");
-		model.addAttribute("address", "東京都新宿区1-2-3");
-		model.addAttribute("phone", "090-1234-1234");
-		model.addAttribute("updateDate", LocalDateTime.now());
-		model.addAttribute("createDate", "2024/05/15 12:00:00");
-		
+	@GetMapping("/user/{id}")
+	public String displayView(@PathVariable Long id, Model model) {
+		User user = userService.findById(id);
+		model.addAttribute("userData", user);
 		return "user/view";
 	}
 }
---
+++ web-app/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> searchAll() {
+		return userRepository.findAll();
+	}
+
+	public User findById(Long id) {
+		return userRepository.findById(id).get();
+	}
+}
